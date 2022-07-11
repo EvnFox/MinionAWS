fb7d4c5076c39a6798a83a869b6a474e603c183a
EvnFox <78891786+EvnFox@users.noreply.github.com>
2022-07-11 09:06:58 -0400

    added gui

---
+++ GUI.py
@@ -0,0 +1,61 @@
+import simplekml
+from MinionAws.TransferManager import TransferManager
+import MinionAws.CsvtoTxt as ct
+import os
+# import filedialog module
+from tkinter import filedialog
+
+#initilize a session
+session = TransferManager('https://sqs.us-east-1.amazonaws.com/728808211862/ICCMO.fifo','minion-data')
+kml = simplekml.Kml()
+
+from tkinter import * 
+
+root = Tk()
+
+root.title('Minion Data')
+session.Dir = root.directory = filedialog.askdirectory() + "\\"
+#root.geometry('1200x600')
+
+e = Label(root, text ='Minion Data', width = 35, borderwidth=5)
+e.grid(row = 0, column = 0, columnspan=3, padx=10, pady = 10)
+
+
+def func():
+    session.queue_to_s3() 
+	
+
+def func1():
+    session.clean_dir()
+    session.access_s3()
+    session.jsontocsv()
+    try:
+            # Try to generate KML files
+            for i in session.devices: 
+
+                location_data = ct.create_files(i, session.Dir)
+                if location_data != []:
+                    lin = kml.newlinestring(name='imei_{}'.format(i),
+                                            coords=location_data)
+                    kml.save('{}\\location.kml'.format(session.Dir))
+    except: 
+        for i in session.devices: 
+            ct.create_files(i, session.Dir)
+           
+def func2():
+	return
+def func3():
+	return
+
+b1 = Button(root, text='Refresh', padx=80, pady=20, command=func)
+b2 = Button(root, text='Download', padx=80, pady=20, command=func1)
+b3 = Button(root, text='Archive', padx=80, pady=20, command=func2)
+b4 = Button(root, text='Unarchive', padx=80, pady=20, command=func3)
+
+
+b1.grid(row = 1, column = 0, columnspan=2)
+b2.grid(row = 2, column = 0, columnspan=2)
+b3.grid(row=3, column=0, columnspan=2)
+b4.grid(row=4, column =0, columnspan=2)
+
+root.mainloop()
