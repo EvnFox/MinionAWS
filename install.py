547bd5c2f3db70a6ff33bc608f8fbe441fd4aca8
EvnFox <78891786+EvnFox@users.noreply.github.com>
2022-07-12 10:16:20 -0400

    added 'aws config'

--- install.py
+++ install.py
@@ -4,11 +4,19 @@ try:
     os.system('python --version')
 except:
     print('install python')
-
+    exit()
 try: 
     os.system('pip --version')
 except: 
     print('install pip')
-    
+    exit()
+try: 
+    os.system('aws')
+except: 
+    print('you need to install aws cli\nrefer to readme.md')
+    exit()
+
 for i in ['pandas', 'simplekml', 'tkinter', 'numpy', 'boto3', 'datetime', 'glob']:
     os.system('pip install {}'.format(i))
+
+os.system('aws configure')
